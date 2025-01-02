4f40d344245ca5b2732a980186b7ffbb2063cb2a
ayyan gharade <ayyangharade05@gmail.com>
2025-01-02 03:14:13 -0800

    new commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
+import Youtube from "./Youtube";
 
 function App() {
   return (
-  
+    <>
     <h1>classic react app</h1>
+    <Youtube/>
+    </>
     
   );
 }
--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Youtube from "./Youtube"
 
 function App() {
   
 
   return (
+<>
   <h1>Vite react App</h1>
+  <h1>Vite react App</h1>
+  <Youtube/>
+</> 
   )
 }
 

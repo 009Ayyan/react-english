4e28f21d3d98b3c091bd939c038efee133f93815
ayyan gharade <ayyangharade05@gmail.com>
2025-01-03 03:58:02 -0800

    counter is created successfully

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter,setCounter]=useState(15) //[variable,Function]
+ // let counter = 15
+
+ const addValue=()=>{
+ setCounter((prevCounter)=>prevCounter + 1)//callback is used to access the previous value 
+ setCounter((prevCounter)=>prevCounter + 1)//it takes the previous value and adds it with 1
+ setCounter((prevCounter)=>prevCounter + 1)
+ setCounter((prevCounter)=>prevCounter + 1)
+ }
+
+ const removeValue = ()=>{
+  setCounter(counter - 1)
+ }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>React course with Hitesh {counter}</h1>
+     <h2> Counter Value:{counter} </h2>
+     <button
+     onClick={addValue}
+     >Add value</button> {" "}
+     <button onClick={removeValue}>remove value</button>
+     <p> footer:{counter}</p>
     </>
   )
 }

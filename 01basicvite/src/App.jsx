d04dc44fdfeaf0face53916debee4598f65a09c1
ayyan gharade <ayyangharade05@gmail.com>
2025-01-03 03:30:23 -0800

    custom react app (link to visit google)and 02counter react app are created

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,14 @@
 import Youtube from "./Youtube"
 
+
+
 function App() {
-  
+  const username = "hitesh";
 
   return (
 <>
-  <h1>Vite react App</h1>
-  <h1>Vite react App</h1>
+  <h1>Vite react App {2*2} </h1>
+  <h1>Vite react App {}</h1>
   <Youtube/>
 </> 
   )
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'; // Ensure React is imported
+import ReactDOM from 'react-dom/client';
+
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
+const reactElement= {
+    type: 'a',
+    props: {
+        href:"https://google.com",
+        target:"_blank"
+    },
+    children: 'click me to visit google'
+}
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom react App</h1>
+        </div>
+    )
+}
+const AnotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target: "_blank"},
+    'click to visit google',
+    " "
+    
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+   <App/>
+
   
 )
